7f27dc1373ad50420bdc1e9b24e4d3659d69db77
mayksel <michael_geronimo@dlsu.edu.ph>
2023-07-17 07:51:41 +0800

    optimize

--- asmproject/simdfunc.asm
+++ asmproject/simdfunc.asm
@@ -12,15 +12,7 @@ dotproductsimd:
 		vmovdqu ymm1, [r8] 
 		vpmulld ymm2, ymm1, ymm0 
 		vphaddd ymm3, ymm2,ymm2 
-
-		vextractf128 xmm1,ymm1,0 
-		vpextrd rbx, xmm1, 0 
-		add rax, rbx
-		vpextrd rbx, xmm1, 1
-		add rax, rbx
-
 		
-
 		add rdx, 32 
 		add r8, 32 
 		loop loop_start
